40c6bf8807b7bee9ca6bd6d5b66ea2c75655d292
Jose Pradenas Navarro <54380196+lunar-cat@users.noreply.github.com>
2021-02-28 21:09:09 -0300

    Added userPlatform & unified game under Game class

--- TODO.md
+++ TODO.md
@@ -3,6 +3,9 @@
 eso permita que la velocidad de la bola sea x negativa, o xpositiva, así
 permitiendo que el disparo inicial sea hacia la izquierda o derecha
 podríamos calcularlo comparando los últimos 2 x del user mouse
+## esto anterior debería poderse hacer con la nueva api del [mouse](https://developer.mozilla.org/en-US/docs/Web/API/Pointer_Lock_API)
 * El user debería ser creado como la bola igual
 * El juego debería crearlos a ambos
 * necesitamos reiniciar el juego cuando se mueve la bola, y restar vida, si no quedan vidas GG
+* El tema de si colisiona a los lados y en qué punto la bola muere, deberíamos cambiarlo una vez cambiemos la plataforma, porque si le hacemos esquinas diagonales, sería un movimiento posiblemente útil, el cual tenemos que ver cómo funciona.
+* ahora que lo pienso, con la nueva API DEL MOUSE, como no solamente te da la coordenada actual, si no que también te da el movimiento en negativo o positivo, eso nos permitiría saber si estás moviéndote de izquierda a derecha, y condicionar lo positivo o negativo del movimiento horizontal de la bola
--- anim.js
+++ anim.js
@@ -1,7 +1,9 @@
-// Game & Ball Classes
+// Global Canvas / Context & Body
 var canvas = document.getElementById('canvas-test');
 var ctx = canvas.getContext('2d');
 var body = document.getElementById('body-uwu');
+
+// Ball Class
 class Ball {
     constructor(xSpeed, ySpeed, damage, color = 'white') {
         this.xSpeed = xSpeed;
@@ -13,6 +15,12 @@ class Ball {
         this.initY;
         this.x;
         this.y;
+    }
+    getCurrentPosition() {
+        return [this.x, this.y];
+    }
+    getNextPosition() {
+        return [this.x + this.xSpeed, this.y + this.ySpeed];
     }
     getInitialPosition() {
         /** @type {CanvasRenderingContext2D}  */
@@ -34,112 +42,108 @@ class Ball {
         this.y += this.ySpeed;
     }
     isBorderColliding() {
-        if (this.x + this.xSpeed < 0 || this.x + this.xSpeed > canvas.width) {
-            this.xSpeed = -this.xSpeed;
+        if (this.x + this.xSpeed - this.radius < 0 || this.x + this.xSpeed + this.radius > canvas.width) {
+            this.changeXspeed();
         }
-        if (this.y + this.ySpeed < 0 || this.y + this.xSpeed > canvas.height) {
-            this.ySpeed = -this.ySpeed;
+        if (this.y + this.ySpeed - this.radius < 0 || this.y + this.xSpeed + this.radius > canvas.height) {
+            this.changeYspeed();
         }
     }
-    delete() { 
+    changeYspeed() {
+        this.ySpeed = -this.ySpeed;
+    }
+    changeXspeed() {
+        this.xSpeed = -this.xSpeed;
+    }
+    delete() {
         /* Lo que ocurre, es como el orden es dibujar, agregar FUTURO x e y, agendar new frame, dibujar... cuando se acaba y muere, el futuro x e y, quedan marcados, dislocando la posición real del actual frame, por lo cual acá resto lo que iba siendo la velocidad, y ahí sí funciona c: */
         /** @type {CanvasRenderingContext2D}  */
         this.x -= this.xSpeed;
         this.y -= this.ySpeed;
 
-        // para debug
-        ctx.moveTo(this.x, this.y);
-        ctx.lineTo(0, 0);
-        ctx.strokeStyle = 'green';  
-        ctx.stroke();
+        // para debug 
+        let line = new Path2D();
+        line.moveTo(this.x, this.y);
+        line.lineTo(0, 0);
+        ctx.strokeStyle = 'green';
+        ctx.stroke(line);
         // para debug
 
         ctx.clearRect(this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2);
     }
     increaseSize() {
-
+        this.radius *= 2;
     }
     decreaseSize() {
-
+        this.radius /= 2;
     }
-    stickToUser() {
-
+    increaseSpeed(x = this.x, y = this.y) {
+        this.x += x;
+        this.y += y;
     }
-    increaseSpeed() {
-
-    }
-    decreaseSpeed() {
-
+    decreaseSpeed(x = this.x, y = this.y) {
+        this.x -= x;
+        this.y -= y;
     }
     increaseDamage() {
-
+        this.damage += 1;
     }
     decreaseDamage() {
-
-    }
-    isColliding() {
-
+        this.damage -= 1;
     }
     isDead() {
-        if (this.y > +canvas.height - this.radius) {
-            window.cancelAnimationFrame(ballID);
+        if (this.y > (this.InitY + this.radius)) { 
             this.delete();
             return true;
         }
         return false;
     }
-    releaseBall() {
-
+    moveOverPlatform(platformX) {
+        this.x = platformX;
     }
 }
-function createBall() {
-    const ball = new Ball(-5, -3, 1);
-    ball.getInitialPosition();
-    ball.draw();
-    return ball;
-}
-const ball = createBall();
-const moveBall = (x) => {
-    ball.x = x;
-    ball.draw();
-}
-let ballID = 0;
-function ballAttachtToUser(event) {
-    ctx.clearRect(0, 0, +canvas.width, +canvas.height);
-    let x = event.clientX - (+body.clientWidth - +canvas.width) / 2;
-    if (x < 0) x = 0;
-    if (x > +canvas.width) x = +canvas.width;
-    moveBall(x);
-}
-function getMouseXposition(funct) {
-    window.addEventListener('mousemove', funct);
-}
-getMouseXposition(ballAttachtToUser);
-moveOnClick();
-cleanListenerMoveMouse(ballAttachtToUser);
 
-
-function moveOnClick() {
-    function algo() {
+class Player {
+    constructor(color = 'black') {
+        this.width = 100;
+        this.height = 15;
+        this.color = color;
+        this.initX;
+        this.initY;
+        this.x;
+        this.y;
+    }
+    getInitialPosition() {
         /** @type {CanvasRenderingContext2D}  */
-        ctx.clearRect(0, 0, +canvas.width, +canvas.height);
-        ball.draw();
-        ball.move();
-        if (!ball.isDead()){
-            ballID = window.requestAnimationFrame(algo);
-        }
+        this.InitX = +canvas.width / 2;
+        this.InitY = +canvas.height - (+canvas.height / 10) + 10 + 4; // los 10 del ball.radius // los +4 por si le ponemos un stroke con 4 de lineWidth, según entiendo se le agrega encima al tamaño normal
+        this.x = this.InitX;
+        this.y = this.InitY;
     }
-    canvas.addEventListener('click', algo, { once: true });
-}
-function cleanListenerMoveMouse(funct) {
-    function clearListener() {
-        window.removeEventListener('mousemove', funct);
+    draw() {
+        /** @type {CanvasRenderingContext2D}  */
+        let platform = new Path2D();
+        platform.rect(this.x - (this.width / 2), this.y, this.width, this.height);
+        ctx.fillStyle = 'gray';
+        ctx.strokeStyle = this.color;
+        ctx.lineWidth = 4;
+        ctx.stroke(platform);
+        ctx.fill(platform);
     }
-    canvas.addEventListener('click', clearListener, { once: true })
+    move(x) {
+        this.x = x - (+body.clientWidth - +canvas.width) / 2;
+        if (this.x < 0) this.x = 0;
+        if (this.x > +canvas.width) this.x = +canvas.width;
+        return this.x; // este return para que la bola siga el movimiento del mouse antes de lanzar
+    }
+    /* Para estas  variables, deberíamos pasar igual el ball.radius en vez de sumar "10"
+    porque si la bola crece, cambia po */
+    ballIsFrontColliding(ballX, ballY, ballRadius) {
+        return (ballX + ballRadius >= this.x - (this.width / 2) && ballX - ballRadius <= this.x + (this.width / 2) && ballY + ballRadius >= this.y);
+    }
 }
 
-
-
+/* podríamos hacer la bola y el user globales en la clase poniendolos como this.ball = ball?XD */
 class Game {
     /* El maxScore lo tomaríamos de localStorage, igual que el current lo mantendríamos
     ahí como tal, hasta que se vuelva el maxScore */
@@ -151,24 +155,87 @@ class Game {
         this.gameLives = gameLives;
         this.gameState = gameState;
         this.ballsAmount = ballsAmount;
+        this.nextFrameID = 0; // esto sería el ballID
+        this.userXposition = 0;
     }
+
     clearCanvas() {
         /** @type {CanvasRenderingContext2D}  */
         ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
-    renderBackground() {
-
-    }
     renderNewGame() {
         /** @type {CanvasRenderingContext2D}  */
+        const ball = this.initBall();
+        const user = this.initPlatform();
+        const drawingFunction = () => {
+            this.drawWaitingGame(ball, user);
+        }
+        this.drawInit(ball, user);
+        this.addEvents(window, 'mousemove', (event) => this.setUserPosition(event));
+        this.addEvents(window, 'mousemove', drawingFunction);
+        this.addEvents(canvas, 'click', this.drawStartedGame.bind(this, ball, user), { once: true });
+        this.addEvents(canvas, 'click', () =>  this.removeEvents(window, 'mousemove', drawingFunction), {once: true});
+    }
+    initBall() {
+        const ball = new Ball(-3, -3, 1);
+        ball.getInitialPosition();
+        return ball;
+    }
+    initPlatform() {
+        const platformUser = new Player();
+        platformUser.getInitialPosition();
+        return platformUser;
+    }
+    setUserPosition(event) {
+        let xUser = event.clientX;
+        this.userXposition = +xUser;
+    }
+    drawInit(ball, platformUser) {
+        ball.draw();
+        platformUser.draw();
+    }
+    drawWaitingGame(ball, user) {
         this.clearCanvas();
-
-        // render background
-        // render blocks
-        // render ball
-        // render user
-        // asigna el número de bolas en juego a 1
-        // asignal el gameRound a 1
+        let x = this.userXposition;
+        let userX = this.drawWaitingUser(user, x);
+        this.drawWaitingBall(ball, userX);
+        user.draw();
+        ball.draw();
+    }
+    drawWaitingBall(ball, x) {
+        ball.moveOverPlatform(x);
+    }
+    drawWaitingUser(user, x) {
+        return user.move(x);
+    }
+    drawStartedGame(ball, user) {
+        this.clearCanvas();
+        if (user.ballIsFrontColliding(...ball.getNextPosition(), ball.radius)) ball.changeYspeed();
+        this.drawStartedBall(ball);
+        this.drawStartedUser(user);
+        if (!ball.isDead(this.nextFrameID)) {
+            this.nextFrameID = window.requestAnimationFrame(this.drawStartedGame.bind(this, ball, user));
+        } else {
+            window.cancelAnimationFrame(this.nextFrameID);
+            window.setTimeout(() => {
+                this.clearCanvas();
+                this.renderNewGame();
+            }, 500);
+        }
+    }
+    drawStartedBall(ball) {
+        ball.draw();
+        ball.move();
+    }
+    drawStartedUser(user) {
+        user.move(this.userXposition);
+        user.draw();
+    }
+    addEvents(objectToAttatch, eventType, eventFunction, eventOptions = {}) {
+        objectToAttatch.addEventListener(eventType, eventFunction, eventOptions);
+    }
+    removeEvents(objectToAttatch, eventType, eventFunction, eventOptions = {}) {
+        objectToAttatch.removeEventListener(eventType, eventFunction, eventOptions);
     }
     isGameOver() {
         // check if all the balls
@@ -199,3 +266,5 @@ class Game {
 
 // UI 
 
+let game = new Game(0, 0, 'uwu', 1, 3, 'continue', 3);
+game.renderNewGame();
