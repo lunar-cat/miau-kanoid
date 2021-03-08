e41839981cf09f2c5594ab35f093ec65dc2ed99d
Jose Pradenas Navarro <54380196+lunar-cat@users.noreply.github.com>
2021-03-08 15:58:23 -0300

    Added Score&Life, refactored class functions

--- TODO.md
+++ TODO.md
@@ -7,9 +7,15 @@ podríamos calcularlo comparando los últimos 2 x del user mouse
 
 
 * necesitamos reiniciar el juego cuando se mueve la bola, y restar vida, si no quedan vidas GG
-* El tema de si colisiona a los lados y en qué punto la bola muere, deberíamos cambiarlo una vez cambiemos la plataforma, porque si le hacemos esquinas diagonales, sería un movimiento posiblemente útil, el cual tenemos que ver cómo funciona.
 
 ## El problema de la detección de colisión es que debería detectar si entre la posición actual y la posición siguiente hay un overlap de x/y para detectar la colisión, porque de lo contrario puede ser que ahora mismo la posición actual no choque, y que la distancia sea de 2px, pero si la velocidad es de 4px, el próximo ya lo atravezará
 
-## Ok ya reacciona mejor, queda mejorar el rendimiento agregando el coso de que detecte 4 áreas grandes primero, y así pueda decidir entre revisar menos bloques
 ## Agregar UI también y ordenar las distintas razones de terminar el juego (perder o ganar)
+
+## debería pasar la current y next position, así veo si ahora y a futuro chocará
+## insisto, necesitamos el side colliding para la plataforma del usuario, porque o si no la bola seguirá atravesando
+
+## podría hacer que si el click es izquierdo o derecho cambie el inicio de la dirección de la bola, haciendo preventDefault en el contextmenu del click derecho en el canvas
+
+## que tal si cambiamos el X,Y de un bloque, y que estos estén en el centro? y así simplemente vemos is colisiona arriba, abajo o a los lados
+## y para ver si está chocando, sumamos la distancia del centro al borde más el radio de la bola, si eso es 0 o menos, pues colisión!
--- anim.js
+++ anim.js
@@ -1,4 +1,4 @@
-// Global Canvas / Context & Body
+// Global Canvas, Context & Body
 var canvas = document.getElementById('canvas-test');
 var ctx = canvas.getContext('2d');
 var body = document.getElementById('body-uwu');
@@ -15,6 +15,8 @@ class Ball {
         this.initY;
         this.x;
         this.y;
+        this.isAlreadyColliding = false;
+        this.isAlreadyFaster = false;
     }
     getCurrentPosition() {
         return [this.x, this.y];
@@ -22,7 +24,7 @@ class Ball {
     getNextPosition() {
         return [this.x + this.xSpeed, this.y + this.ySpeed];
     }
-    getInitialPosition() {
+    setInitialPosition() {
         /** @type {CanvasRenderingContext2D}  */
         this.InitX = +canvas.width / 2;
         this.InitY = +canvas.height - (+canvas.height / 10);
@@ -35,6 +37,7 @@ class Ball {
         ball.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true); // asumiendo que no conocieramos el exacto width y heigth usamos porcentaje?
         ctx.fillStyle = this.color;
         ctx.fill(ball);
+        this.isAlreadyColliding = false;
     }
     move() {
         this.isBorderColliding();
@@ -42,12 +45,8 @@ class Ball {
         this.y += this.ySpeed;
     }
     isBorderColliding() {
-        if ((this.x) <= this.radius ||(canvas.width - this.x) <= this.radius) {
-            this.changeXspeed();
-        }
-        if ((this.y) <= this.radius || (canvas.height - this.y) <= this.radius) {
-            this.changeYspeed();
-        }
+        if ((this.x) <= this.radius || (canvas.width - this.x) <= this.radius) this.changeXspeed();
+        if ((this.y) <= this.radius || (canvas.height - this.y) <= this.radius) this.changeYspeed();
     }
     changeYspeed() {
         this.ySpeed = -this.ySpeed;
@@ -60,15 +59,13 @@ class Ball {
         /** @type {CanvasRenderingContext2D}  */
         this.x -= this.xSpeed;
         this.y -= this.ySpeed;
-
         // para debug 
-        let line = new Path2D();
-        line.moveTo(this.x, this.y);
-        line.lineTo(0, 0);
-        ctx.strokeStyle = 'green';
-        ctx.stroke(line);
+        /*         let line = new Path2D();
+                line.moveTo(this.x, this.y);
+                line.lineTo(0, 0);
+                ctx.strokeStyle = 'green';
+                ctx.stroke(line); */
         // para debug
-
         ctx.clearRect(this.x - this.radius - 5, this.y - this.radius - 5, this.radius * 2 + 10, this.radius * 2 + 10);
     }
     increaseSize() {
@@ -77,11 +74,16 @@ class Ball {
     decreaseSize() {
         this.radius /= 2;
     }
-    increaseSpeed(x = this.x, y = this.y) {
-        this.x += x;
-        this.y += y;
+    increaseSpeed() {
+        this.xSpeed += (this.xSpeed < 0)
+            ? -1
+            : 1;
+        this.ySpeed += (this.ySpeed < 0)
+            ? -1
+            : 1;
+        this.isAlreadyFaster = true;
     }
-    decreaseSpeed(x = this.x, y = this.y) {
+    decreaseSpeed() {
         this.x -= x;
         this.y -= y;
     }
@@ -103,8 +105,9 @@ class Ball {
     }
 }
 
+// Player Platform Class
 class Player {
-    constructor(color = 'black') {
+    constructor(color = 'darkBlue') {
         this.width = 100;
         this.height = 15;
         this.color = color;
@@ -113,10 +116,10 @@ class Player {
         this.x;
         this.y;
     }
-    getInitialPosition() {
+    setInitialPosition() {
         /** @type {CanvasRenderingContext2D}  */
         this.InitX = +canvas.width / 2;
-        this.InitY = +canvas.height - (+canvas.height / 10) + 10 + 4; // los 10 del ball.radius // los +4 por si le ponemos un stroke con 4 de lineWidth, según entiendo se le agrega encima al tamaño normal
+        this.InitY = +canvas.height - (+canvas.height / 10) + 10 + 4; // los 10 del ball.radius  & los +4 por si le ponemos un stroke con 4 de lineWidth
         this.x = this.InitX;
         this.y = this.InitY;
     }
@@ -124,7 +127,7 @@ class Player {
         /** @type {CanvasRenderingContext2D}  */
         let platform = new Path2D();
         platform.rect(this.x - (this.width / 2), this.y, this.width, this.height);
-        ctx.fillStyle = 'gray';
+        ctx.fillStyle = 'white';
         ctx.strokeStyle = this.color;
         ctx.lineWidth = 4;
         ctx.stroke(platform);
@@ -136,12 +139,11 @@ class Player {
         if (this.x > +canvas.width) this.x = +canvas.width;
         return this.x; // este return para que la bola siga el movimiento del mouse antes de lanzar
     }
-    /* Para estas  variables, deberíamos pasar igual el ball.radius en vez de sumar "10"
-    porque si la bola crece, cambia po */
     ballIsFrontColliding(ballX, ballY, ballRadius) {
         let userX = this.x - this.width / 2;
-        let sideX, sideY;
+        let sideX, sideY, collideType;
         if (ballY < (canvas.height / 4) * 3) return false;
+
         if (ballX < userX) sideX = userX;
         else if (ballX > userX + this.width) sideX = userX + this.width;
         if (ballY < this.y) sideY = this.y;
@@ -149,24 +151,20 @@ class Player {
 
         const distanceX = ballX - sideX || 0;
         const distanceY = ballY - sideY || 0;
+        collideType = (distanceX === 0) ? 'y' : 'x';
         const totalDistance = Math.sqrt((distanceX * distanceX) + (distanceY * distanceY));
         if (totalDistance <= ballRadius) {
-            return true;
+            return true; // agregar el collideType al return como array
         }
-        return  false;
+        return false;
     }
 }
 
 class Game {
 
-    constructor(currentScore = 0, currentName = 'player', gameRound = 1, gameLives = 3, ballsAmount = 1) {
-        this.maxScore;
-        this.nameMaxScore;
-        this.currentScore = currentScore;
-        this.currentName = currentName;
-        this.gameRound = gameRound;
+    constructor(gameLives = 3) {
+        this.currentScore;
         this.gameLives = gameLives;
-        this.ballsAmount = ballsAmount;
         this.nextFrameID = 0; // esto sería el ballID
         this.userXposition = 0;
         this.ball;
@@ -177,76 +175,92 @@ class Game {
         /** @type {CanvasRenderingContext2D}  */
         ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
-    renderNewGame() {
-        /** @type {CanvasRenderingContext2D}  */
-        this.bricks = [];
-        const ball = this.initBall(-4, -4, 1);
-        const user = this.initPlatform();
-        const brick = this.initBricks();
-        this.bricks.push(...brick);
+    clearBottomCanvas() {
+        ctx.clearRect(0, canvas.height / 2, canvas.width, canvas.height / 2);
+    }
+    renderGame(){
         const drawingFunction = () => this.drawWaitingGame();
-        this.ball = ball;
-        this.userPlatform = user;
         this.drawInit();
         this.addEvents(window, 'mousemove', event => this.setUserPosition(event));
         this.addEvents(window, 'mousemove', drawingFunction);
         this.addEvents(canvas, 'click', () => this.drawStartedGame(), { once: true });
         this.addEvents(canvas, 'click', () => this.removeEvents(window, 'mousemove', drawingFunction), { once: true });
+    }
+    initGame(type = null) {
+        const ball = this.initBall(-4, -4, 1);
+        const user = this.initPlatform();
+        this.ball = ball;
+        this.userPlatform = user;
+        if (type === "new") {
+            const bricksNodes = this.initBricks();
+            this.bricks = bricksNodes;
+        }
+    }
+    renderNewGame() {
+        this.addHighScoreOnNewGame();
+        this.UIresetLifes();
+        this.initGame('new');
+        this.renderGame();
     }
     continueGame() {
-        const ball = this.initBall(-4, -4, 1);
-        const user = this.initPlatform();
-        const drawingFunction = () => this.drawWaitingGame();
-        this.ball = ball;
-        this.userPlatform = user;
-        this.drawInit();
-        this.addEvents(window, 'mousemove', event => this.setUserPosition(event));
-        this.addEvents(window, 'mousemove', drawingFunction);
-        this.addEvents(canvas, 'click', () => this.drawStartedGame(), { once: true });
-        this.addEvents(canvas, 'click', () => this.removeEvents(window, 'mousemove', drawingFunction), { once: true });
+        this.initGame();
+        this.renderGame();
     }
     initBricks() {
-        let bricksColors = ['yellow', 'brown', 'purple', 'orange', 'white'];
-        let bricksArray = [];
+        const [topLeft, topRight, bottomLeft, bottomRight ] = [[], [], [], []];
+        const bricksNodes = {
+            topLeftNode: topLeft,
+            topRightNode: topRight,
+            bottomLeftNode: bottomLeft,
+            bottomRightNode: bottomRight,
+        };
+        this.createBricks(topLeft, topRight, bottomLeft, bottomRight);
+        return bricksNodes;
+    }
+    createBricks(topLeft, topRight, bottomLeft, bottomRight){
+        let bricksColors = [[64, 100, 50], [117, 100, 15], [298, 100, 50], [30, 100, 50], [0, 0, 99]]; // colors on HSL format
+        let brickDurability = [3, 2, 2, 1, 4];
         for (let index = 1; index <= 8; index++) {
             for (let Yindex = 1; Yindex <= 5; Yindex++) {
                 let x = 5;
                 let y = 10;
                 let mult = 60;
-                let brick = new Brick(1, 50, 20, (x + (mult * index)), y + (mult * Yindex), bricksColors[Yindex - 1]);
-                bricksArray.push(brick);
-            }
-
-        }
-        return bricksArray;
+                let brick = new Brick(brickDurability[Yindex - 1], 50, 30, (x + (mult * index)), y + (mult * Yindex), bricksColors[Yindex - 1]);
+                if (brick.x < canvas.width / 2) {
+                    if (brick.y < canvas.height / 3.5) topLeft.push(brick);
+                    else bottomLeft.push(brick);
+                } else if (brick.x > canvas.width / 2) { // derecha
+                    if (brick.y < canvas.height / 3.5) topRight.push(brick);
+                    else bottomRight.push(brick);
+                };
+            };
+        };
     }
     initBall(xSpeed, ySpeed, dmg) {
         const ball = new Ball(xSpeed, ySpeed, dmg);
-        ball.getInitialPosition();
+        ball.setInitialPosition();
         return ball;
     }
     initPlatform() {
         const platformUser = new Player();
-        platformUser.getInitialPosition();
+        platformUser.setInitialPosition();
         return platformUser;
     }
     setUserPosition(event) {
-        let xUser = event.clientX;
-        this.userXposition = +xUser;
+        this.userXposition = +event.clientX;
     }
     drawInit() {
         this.ball.draw();
         this.userPlatform.draw();
-        this.bricks.forEach(brick => brick.draw());
+        Object.values(this.bricks).flat().forEach(brick => brick.draw());
     }
     drawWaitingGame() {
-        this.clearCanvas();
+        this.clearBottomCanvas();
         let x = this.userXposition;
         let userX = this.drawWaitingUser(x);
         this.drawWaitingBall(userX);
         this.userPlatform.draw();
         this.ball.draw();
-        this.bricks.forEach(brick => brick.draw());
     }
     drawWaitingBall(x) {
         this.ball.moveOverPlatform(x);
@@ -255,20 +269,45 @@ class Game {
         return this.userPlatform.move(x);
     }
     drawStartedGame() {
+        let closestNode;
+
+        // clean & draw
         this.clearCanvas();
         this.drawStartedBall(this.ball);
         this.drawStartedUser(this.userPlatform);
+        Object.values(this.bricks).flat().forEach(brick => brick.draw());
+
+        if (Object.values(this.bricks).flat().length === 24 && !this.ball.isAlreadyFaster) this.ball.increaseSpeed();
         if (this.userPlatform.ballIsFrontColliding(...this.ball.getNextPosition(), this.ball.radius)) this.ball.changeYspeed();
-        this.bricks.forEach(brick => brick.draw());
-        this.bricks.forEach(brick => {
-            let collision = brick.ballIsColliding(...this.ball.getNextPosition(), this.ball.radius);
-            if (collision[0]) {
-                if (collision[1] === 'x') this.ball.changeXspeed();
-                else this.ball.changeYspeed();
-                let index = this.bricks.indexOf(brick);
-                this.bricks.splice(index, 1);
+
+        if (this.ball.y < canvas.height / 2) { // Si está en el lado de arriba, donde están los bloques 
+            if (this.ball.x < canvas.width / 2) { // revisa la izquierda
+                closestNode = (this.ball.y < canvas.height / 3.5)
+                    ? "topLeftNode" : "bottomLeftNode";
+            } else { // revisa la derecha
+                closestNode = (this.ball.y < canvas.height / 3.5)
+                    ? "topRightNode" : "bottomRightNode";
             }
-        });
+            for (let index = 0; index < this.bricks[closestNode].length; index++) {
+                let brick = this.bricks[closestNode][index];
+                let collision = brick.ballIsColliding(...this.ball.getNextPosition(), this.ball.radius);
+                if (collision[0]) {
+                    if (!this.ball.isAlreadyColliding) {
+                        if (collision[1] === 'x') this.ball.changeXspeed();
+                        else this.ball.changeYspeed();
+                    }
+                    this.ball.isAlreadyColliding = true;
+                    brick.reduceLife(this.ball.damage);
+                    if (brick.durability === 0) {
+                        let index = this.bricks[closestNode].indexOf(brick);
+                        this.bricks[closestNode].splice(index, 1);
+                        brick.delete();
+                    } else {
+                        brick.drawDamaged()
+                    }
+                }
+            }
+        }
         if (!this.ball.isDead(this.nextFrameID) && !this.isWin()) {
             this.nextFrameID = window.requestAnimationFrame(() => this.drawStartedGame());
         } else {
@@ -286,7 +325,8 @@ class Game {
             }
         }
     }
-    finishGame(){
+    finishGame() {
+        this.updateScore();
         this.clearCanvas();
         this.renderNewGame();
     }
@@ -305,53 +345,95 @@ class Game {
         objectToAttatch.removeEventListener(eventType, eventFunction, eventOptions);
     }
     isGameOver() {
-        return (this.gameLives === 0);
+        if (this.gameLives <= 0){
+            this.gameLives = 3;
+            return true;
+        }
     }
-    isWin(){
-        return (this.bricks.length === 0);
-    }
-    addLife() {
-        this.gameLives += 1;
+    isWin() {
+        return (Object.values(this.bricks).flat().length === 0);
     }
     removeLife() {
         this.gameLives -= 1;
+        this.UIlifes(1);
     }
-    changeName(newName) {
-        this.changeName = newName.replace(/\W/g, '').slice(0, 5);
+    updateScore(){
+        const highestScore = this.getMaxScore();
+        const currentScore = this.getCurrentScore();
+        if (+currentScore > +highestScore){
+            this.UIHighScore(currentScore);
+        }
+        this.addNewScore(currentScore);
+        this.UICurrentScore(currentScore);
     }
-    addNewMaxScore() {
-        // Acá agregaríamos una función para cambiarlo en la UI también
-        localStorage.setItem(this.currentName, this.currentScore);
+    addNewScore(score) {
+        let index = localStorage.length
+        localStorage.setItem(`user${index}`, score);
     }
-    updateCurrentScore(score) {
-        this.currentScore = score;
+    getCurrentScore(){
+        let bricksAlive = Object.values(this.bricks).flat().length;
+        let score = (40 - bricksAlive) * 250;
+        return score;
     }
-    getMaxScore() { // se supone que el entrie sería del tipo [ ['uwu', 10], ['awa', 20]   ]
-        const scores = Object.entries(localStorage).sort((a, b) => a[1] - b[1]);
-        this.nameMaxScore = scores[0][0];
-        this.maxScore = scores[0][1];
+    getMaxScore() {
+        let score = 0;
+        if (localStorage.length > 0) {
+            const scores = Object.entries(localStorage).sort((a, b) => +b[1] - +a[1]);
+            score = +scores[0][1];
+        }
+        return score;
+    }
+    addHighScoreOnNewGame(){
+        let score = this.getMaxScore();
+        this.UIHighScore(score);
+    }
+    UIHighScore(score){
+        const UI = document.getElementById('high-score-points');
+        UI.textContent = score;
+    }
+    UICurrentScore(score){
+        const UI = document.getElementById('current-score-points');
+        UI.textContent = score;
+    }
+    UIlifes(amount){
+        const UI = document.getElementById('user-life-points');
+        const oldValue = UI.textContent;
+        UI.textContent = +oldValue - amount;
+    }
+    UIresetLifes(){
+        const UI = document.getElementById('user-life-points');
+        UI.textContent = 3;
     }
 }
 
 class Brick {
-    constructor(durability = 1, width = 50, height = 20, x, y, brickColor) {
+    constructor(durability = 1, width = 50, height = 20, x, y, colors) {
         this.durability = durability;
         this.width = width;
         this.height = height;
         this.x = x;
         this.y = y;
-        this.brickColor = brickColor;
+        this.colorH = colors[0];
+        this.colorS = colors[1];
+        this.colorL = colors[2];
     }
     draw() {
         let brick = new Path2D();
         brick.rect(this.x, this.y, this.width, this.height);
         /** @type {CanvasRenderingContext2D}  */
-        ctx.fillStyle = this.brickColor;
+        let brickColor = `hsl(${this.colorH}, ${this.colorS}%, ${this.colorL}%)`;
+        ctx.fillStyle = brickColor;
         ctx.shadowColor = 'black';
         ctx.shadowOffsetX = 2;
         ctx.shadowOffsetY = 5;
         ctx.fill(brick);
         ctx.shadowColor = 'transparent';
+    }
+    drawDamaged() {
+        this.colorL -= 15;
+    }
+    reduceLife(dmg) {
+        this.durability -= dmg;
     }
     ballIsColliding(ballX, ballY, ballRadius) {
         let sideX, sideY, typeOfCollide;
@@ -366,7 +448,6 @@ class Brick {
         typeOfCollide = (distanceX === 0) ? 'y' : 'x';
         const totalDistance = Math.sqrt((distanceX * distanceX) + (distanceY * distanceY));
         if (totalDistance <= ballRadius) {
-            this.delete();
             return [true, typeOfCollide];
         }
         return [false, typeOfCollide];
@@ -376,8 +457,5 @@ class Brick {
     }
 }
 
-// UI // quizá deberíamos crear una nueva variable con el diámetro y no solo con el radio de la bola
-
-
-let game = new Game(0, 'uwu', 1, 3, 1);
+let game = new Game(3);
 game.renderNewGame();
